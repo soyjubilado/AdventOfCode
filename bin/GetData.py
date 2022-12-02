a2cd86d03a3f6aa164f78c7cb1f2e7213be82147
WandaMinimoff <maxima@freeshell.org>
2022-12-02 13:29:29 -0800

    separate config data from code

--- bin/GetData.py
+++ bin/GetData.py
@@ -6,7 +6,10 @@ Grab the data for a given day and put it in file dataDDYYYY.txt
 Usage: GetData.py <day> <year>
 
  * Create data<YEAR><DAY>.txt
- * Requires valid session cookie in file SessionCookie.
+ * Requires the following definitions in GetDataConfig.py:
+    - SESSION_COOKIE: an adventofcode session cookie
+    - CONTACT: an email address for the user agent
+    - TEST_URL: a test URL
  * Pulls data via curl, with contact info in the user agent field.
  * Test with 'GetData.py 0 0' which will try to pull from TEST_URL.
 """
@@ -14,21 +17,12 @@ Usage: GetData.py <day> <year>
 import os
 import subprocess
 import sys
+from GetDataConfig import SESSION_COOKIE, CONTACT, TEST_URL
 
-# look at your chrome cookies from AOC web site
-# inspect -> application -> cookies -> session
 
-COOKIE_FILE = f'{os.path.dirname(__file__)}/SessionCookie'
-TEST_URL = 'redacted'
-CONTACT = 'redacted'
-YEAR = 2022
+CURL = '/usr/bin/curl'
 DAY = 1
-
-
-def ReadCookie(cookie_file):
-  with open(cookie_file, 'r') as fh:
-    cookie = next(fh).strip()
-  return cookie
+YEAR = 2022
 
 
 def GetUrl(day=0, year=0):
@@ -40,15 +34,15 @@ def GetUrl(day=0, year=0):
 
 def UserAgent():
   """Return a UserAgent string. curl version determined dynamically."""
-  cmd = ['/usr/bin/curl', '-V']
+  cmd = [CURL, '-V']
   raw_data = subprocess.check_output(cmd).decode('utf-8').strip()
   curl_version = raw_data.split()[1]
-  return f'curl/{curl_version} from {CONTACT}' 
+  return f'curl/{curl_version} from {CONTACT}'
 
 
 def GetRawInput(session_cookie, target_url):
   """use curl to get input"""
-  cmd = ['/usr/bin/curl',
+  cmd = [CURL,
          target_url,
          '-X', 'GET', '-H',
          f'Cookie: session={session_cookie}',
@@ -65,16 +59,15 @@ def main(argv):
     print(f'Usage: {os.path.basename(__file__)} <day> [<year>]')
     sys.exit()
   day = DAY if len(argv) <= 1 else int(argv[1])
-  year = YEAR if len(argv) <=2 else int(argv[2])
+  year = YEAR if len(argv) <= 2 else int(argv[2])
   filename = f'data{year}{day:>02}.txt'
   if os.path.exists(filename):
     print(f'{filename} already exists')
     sys.exit()
 
   with open(filename, 'w') as fh:
-    aoc_cookie = ReadCookie(COOKIE_FILE)
     target_url = GetUrl(day, year)
-    fh.write(GetRawInput(aoc_cookie, target_url))
+    fh.write(GetRawInput(SESSION_COOKIE, target_url))
     fh.write('\n')
 
   # create empty file if it doesn't exit
