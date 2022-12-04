eb1d68bebf5f3e068974f77da4df7c03ce5a1bac
WandaMinimoff <maxima@freeshell.org>
2022-12-04 03:29:00 -0800

    combine parts 1 and 2 to one function

--- 2022/prog202204.py
+++ 2022/prog202204.py
@@ -5,14 +5,14 @@
 DATA = 'data202204.txt'
 
 
-def GetData(datafile):
+def GetData(datafile) -> list:
   lines = []
   with open(datafile, 'r') as fh:
     lines = [i.strip() for i in fh]
   return lines
 
 
-def ParseLineToNumbers(line):
+def ParseLineToNumbers(line) -> tuple:
   """'1-2,3-4' -> (1,2,3,4)"""
   one, two = line.split(',')
   one_s, one_f = [int(i) for i in one.split('-')]
@@ -20,13 +20,13 @@ def ParseLineToNumbers(line):
   return one_s, one_f, two_s, two_f
 
 
-def WhollyContained(one_s, one_f, two_s, two_f):
+def WhollyContained(one_s, one_f, two_s, two_f) -> bool:
   """One range is fully within the other."""
   return (one_s <= two_s <= two_f <= one_f or
           two_s <= one_s <= one_f <= two_f)
 
 
-def OverlapAtAll(one_s, one_f, two_s, two_f):
+def OverlapAtAll(one_s, one_f, two_s, two_f) -> bool:
   """The two ranges overlap at least partially."""
   return (one_s <= two_s <= one_f or
           one_s <= two_f <= one_f or
@@ -34,29 +34,25 @@ def OverlapAtAll(one_s, one_f, two_s, two_f):
           two_s <= one_f <= two_f)
 
 
-def Part1(lines):
+def Solve(lines, part) -> None:
+  """Print out the answer.
+  Args:
+    lines: (list of str) lines of input
+    part: 'Part 1' or 'Part 2'
+  """
   overlaps = []
+  CriteriaMet = WhollyContained if part == 'Part 1' else OverlapAtAll
   for l in lines:
     one_s, one_f, two_s, two_f = ParseLineToNumbers(l)
-    if WhollyContained(one_s, one_f, two_s, two_f):
+    if CriteriaMet(one_s, one_f, two_s, two_f):
       overlaps.append((one_s, one_f, two_s, two_f))
-  print(f'Part 1: {len(overlaps)}')
-
-
-def Part2(lines):
-  """The two ranges overlap at least partially."""
-  overlaps = []
-  for l in lines:
-    one_s, one_f, two_s, two_f = ParseLineToNumbers(l)
-    if OverlapAtAll(one_s, one_f, two_s, two_f):
-      overlaps.append((one_s, one_f, two_s, two_f))
-  print(f'Part 2: {len(overlaps)}')
+  print(f'{part}: {len(overlaps)}')
 
 
 def main():
   lines = GetData(DATA)
-  Part1(lines)
-  Part2(lines)
+  Solve(lines, 'Part 1')
+  Solve(lines, 'Part 2')
 
 
 if __name__ == '__main__':
