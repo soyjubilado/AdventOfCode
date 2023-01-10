2948eb317ee1be0eca9f3e6f2f0e8ba58b987e79
soyjubilado <maxima@freeshell.org>
2023-01-10 09:16:39 -0800

    stop using temp files

--- 2022/prog202222_part3.py
+++ 2022/prog202222_part3.py
@@ -4,55 +4,47 @@
 """
 
 import os
-from prog202222 import Solve
+from prog202222 import Solve, GetData
 
 # download the file and put the file name here.
 PART3_DATA = 'data202222_part3.txt'
 
 
-def SplitIntoSubFiles(filename):
-  """Split filename into a bunch of smaller files."""
+def GetAllCases(filename):
+  """Return a list of lists; each sub list is the equivalent of GetData()."""
 
   with open(filename) as fh:
     all_lines = fh.readlines()
   lines = iter(all_lines)
-  counter = 0
-  prefix = 'craycray_'
-  fname = f'{prefix}{counter:>02}.txt'
-  all_files = []
+  all_cases= []
 
   while True:
-    all_files.append(fname)
-    # If filename exists already, raise assertion error
-    assert not os.path.isfile(fname)
-    with open(fname, 'w') as fh:
-      try:
+    this_case = []
+    try:
+      line = next(lines)
+      while len(line.strip()) != 0:
+        this_case.append(line.rstrip())
         line = next(lines)
-        while len(line.strip()) != 0:
-          fh.write(line)
-          line = next(lines)
-        fh.write(line)     # print the blank
-        line = next(lines) # get the next line
-        fh.write(line)     # print that one
-        _ = next(lines)
-      except StopIteration:
-        break
-    counter += 1
-    fname = f'{prefix}{counter:>02}.txt'
-
-  return all_files
+      this_case.append(line.rstrip())  # print the blank
+      line = next(lines)               # get the next line
+      this_case.append(line.rstrip())  # print that one
+      all_cases.append(this_case)
+      _ = next(lines)
+    except StopIteration:
+      break
+  return all_cases
 
 
 def main():
   """main"""
-  files = SplitIntoSubFiles(PART3_DATA)
+  cases = GetAllCases(PART3_DATA)
   running_total = 0
-  for f in files:
-    subtotal = Solve('Part 2', f)
+  counter = 0
+  for lines in cases:
+    subtotal = Solve('Part 2', lines)
     running_total += subtotal
-    print(f'{f}: {subtotal}, running total: {running_total}')
-    # remove each file when done
-    os.unlink(f)
+    print(f'{counter:>2}: {subtotal:>6}, running total: {running_total}')
+    counter += 1
 
 
 if __name__ == '__main__':
