6dd7fd436baeab577ee82284d8ead0423742b4aa
soyjubilado <maxima@freeshell.org>
2023-03-21 13:36:38 -0700

    Put test states inside methods instead of inside class.

--- 2021/tests202123.py
+++ 2021/tests202123.py
@@ -14,16 +14,16 @@ class UnitTestFailure(Exception):
 
 class TestProg202123(unittest.TestCase):
   """Unit tests class."""
-  STATE1 = '''\
-           #############
-           #...........#
-           ###A#D#A#B###
-             #C#C#D#B#
-             #########'''
 
   def testLinesToState(self):
     """Test LinesToState"""
-    lines = dedent(self.STATE1).split('\n')
+    state1 = '''\
+             #############
+             #...........#
+             ###A#D#A#B###
+               #C#C#D#B#
+               #########'''
+    lines = dedent(state1).split('\n')
     expected = set([((2, 1), 'A'),
                     ((2, 2), 'C'),
                     ((4, 1), 'D'),
@@ -36,17 +36,16 @@ class TestProg202123(unittest.TestCase):
 
     self.assertTrue(LinesToState(lines) == expected)
 
-
-  STATE2 = '''\
-           #############
-           #.D.......A.#
-           ###C#.#C#D###
-             #A#B#B#D#
-             #########'''
-
   def testAlreadyHome(self):
     """Test AlreadyHome"""
-    lines = dedent(self.STATE2).split('\n')
+    state2 = '''\
+             #############
+             #.D.......A.#
+             ###C#.#C#D###
+               #A#B#B#D#
+               #########'''
+
+    lines = dedent(state2).split('\n')
     state = LinesToState(lines)
     self.assertEqual(AlreadyHome(((1, 0), 'D'), state), False)
     self.assertEqual(AlreadyHome(((2, 1), 'C'), state), False)
@@ -58,19 +57,15 @@ class TestProg202123(unittest.TestCase):
     self.assertEqual(AlreadyHome(((9, 0), 'A'), state), False)
     self.assertEqual(AlreadyHome(((8, 1), 'D'), state), True)
 
-
-  """
-  STATE2 = '''\
-           #############
-           #.D.......A.#
-           ###C#.#C#D###
-             #A#B#B#D#
-             #########'''
-  """
-
   def testForeignersOccupyHome(self):
     """Test ForeignersOccupyHome"""
-    lines = dedent(self.STATE2).split('\n')
+    state2 = '''\
+             #############
+             #.D.......A.#
+             ###C#.#C#D###
+               #A#B#B#D#
+               #########'''
+    lines = dedent(state2).split('\n')
     state = LinesToState(lines)
     self.assertEqual(ForeignersOccupyHome(((1, 0), 'D'), state), False)
     self.assertEqual(ForeignersOccupyHome(((2, 1), 'C'), state), True)
@@ -82,19 +77,15 @@ class TestProg202123(unittest.TestCase):
     self.assertEqual(ForeignersOccupyHome(((9, 0), 'A'), state), True)
     self.assertEqual(ForeignersOccupyHome(((8, 1), 'D'), state), False)
 
-
-  """
-  STATE2 = '''\
-           #############
-           #.D.......A.#
-           ###C#.#C#D###
-             #A#B#B#D#
-             #########'''
-  """
-
   def testBlockedInTrench(self):
     """Test BlockedInTrench"""
-    lines = dedent(self.STATE2).split('\n')
+    state2 = '''\
+             #############
+             #.D.......A.#
+             ###C#.#C#D###
+               #A#B#B#D#
+               #########'''
+    lines = dedent(state2).split('\n')
     state = LinesToState(lines)
     self.assertEqual(BlockedInTrench(((1, 0), 'D'), state), False)
     self.assertEqual(BlockedInTrench(((2, 1), 'C'), state), False)
@@ -106,10 +97,15 @@ class TestProg202123(unittest.TestCase):
     self.assertEqual(BlockedInTrench(((9, 0), 'A'), state), False)
     self.assertEqual(BlockedInTrench(((8, 1), 'D'), state), False)
 
-
   def testGetOccupiedDict(self):
     """Test GetOccupiedDict"""
-    lines = dedent(self.STATE2).split('\n')
+    state2 = '''\
+             #############
+             #.D.......A.#
+             ###C#.#C#D###
+               #A#B#B#D#
+               #########'''
+    lines = dedent(state2).split('\n')
     state = LinesToState(lines)
     expected = {(1, 0): 'D',
                 (2, 1): 'C',
@@ -124,7 +120,6 @@ class TestProg202123(unittest.TestCase):
     actual = GetOccupiedDict(state)
     self.assertEqual(actual, expected)
 
-
   def testNumbersBetween(self):
     """Test NumbersBetween"""
     self.assertEqual(NumbersBetween(0, 0), [])
@@ -135,17 +130,15 @@ class TestProg202123(unittest.TestCase):
     self.assertEqual(NumbersBetween(1, 4), [2, 3])
     self.assertEqual(NumbersBetween(4, 1), [2, 3])
 
-
-  STATE3 = '''\
-           #############
-           #.D.C.....A.#
-           ###.#.#C#D###
-             #A#B#B#D#
-             #########'''
-
   def testBlockedOutside(self):
     """Test BlockedOutside"""
-    lines = dedent(self.STATE3).split('\n')
+    state3 = '''\
+             #############
+             #.D.C.....A.#
+             ###.#.#C#D###
+               #A#B#B#D#
+               #########'''
+    lines = dedent(state3).split('\n')
     state = LinesToState(lines)
     self.assertEqual(BlockedOutside(((1, 0), 'D'), state), True)
     self.assertEqual(BlockedOutside(((3, 0), 'C'), state), False)
