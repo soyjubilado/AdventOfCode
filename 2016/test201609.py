c859252e72cfd2bda3022cabf9583037af153292
soyjubilado <maxima@freeshell.org>
2023-04-09 19:53:30 -0700

    add tests for ExtractParts()

--- 2016/test201609.py
+++ 2016/test201609.py
@@ -2,7 +2,7 @@
 """Unit tests for prog201609.py"""
 
 import unittest
-from prog201609 import Unpack, Length
+from prog201609 import Unpack, Length, ExtractParts
 
 
 class Tests2016Day09(unittest.TestCase):
@@ -28,6 +28,15 @@ class Tests2016Day09(unittest.TestCase):
     self.assertEqual(Length('(27x12)(20x12)(13x14)(7x10)(1x12)A'), 241920)
     self.assertEqual(Length('(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)'
                             'TWO(5x7)SEVEN'), 445)
+
+  def testExtractParts(self):
+    """Test ExtractParts()"""
+    self.assertEqual(ExtractParts("A(1x5)BC"), ('A', 5, 'B', 'C'))
+    self.assertEqual(ExtractParts("(3x3)XYZ"), ('', 3, 'XYZ', ''))
+    self.assertEqual(ExtractParts("A(2x2)BCD(2x2)EFG"),
+                     ('A', 2, 'BC', 'D(2x2)EFG'))
+    self.assertEqual(ExtractParts("(6x1)(1x3)A"), ('', 1, '(1x3)A', ''))
+    self.assertEqual(ExtractParts("X(8x2)(3x3)ABCY"), ('X', 2, '(3x3)ABC', 'Y'))
 
 
 if __name__ == '__main__':
