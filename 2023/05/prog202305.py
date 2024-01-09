d9fd021caff96781d49647af4f555ea0f013a4fa
soyjubilado <maxima@freeshell.org>
2024-01-09 09:53:19 -0800

    some cleanup

--- 2023/05/prog202305.py
+++ 2023/05/prog202305.py
@@ -14,10 +14,6 @@ DATA = 'data202305.txt'
 TransformMap = namedtuple('transform_map', 'dest_start, src_start, range_len')
 
 
-def Nop(*args, **kwargs):
-  """Null function to suppress printing sometimes."""
-
-
 def GetData(datafile):
   """Read input into a list of lines."""
   lines = []
@@ -49,9 +45,8 @@ class Transformer:
     return answer
 
   def TransformRange(self, t_range_list):
-    # for part 2
-    """Given a single range (start, length), return a single range that
-       represents the transforms for input range.
+    """(For part 2) Given a single range (start, length), return a single
+       range that represents the transforms for input range.
 
        Assumes that the ranges have been cleaned up, and they do not
        cross transform map borders.
@@ -59,10 +54,6 @@ class Transformer:
     answers = []
     for t_range in t_range_list:
       first, length = t_range
-      last = first + length - 1
-      t_first = self.IsInInterval(first) # transform for the first value
-      t_last = self.IsInInterval(last)   # transform for the last value
-      assert t_first == t_last
       new_first = self.Transform(first)
       answers.append((new_first, length))
     return answers
@@ -76,18 +67,19 @@ class Transformer:
         return t
     return False
 
-  def RangesNotContiguous(self):
+  def MakeRangesContiguous(self):
     """Check if all the maps in self are contiguous; if not, return a list
        of additional maps that would make them contiguous"""
     ranges = sorted(self.maps, key=lambda x: x.src_start)
-    problems = []
+    fixes = []
     for i in range(1, len(ranges)):
       r_prev = ranges[i-1]
       r_this = ranges[i]
       if r_prev.src_start + r_prev.range_len != r_this.src_start:
-        problems.append(FixRange(r_prev.src_start, r_prev.range_len,
-                                 r_this.src_start))
-    return problems if problems else False
+        fixes.append(FixRange(r_prev.src_start, r_prev.range_len,
+                              r_this.src_start))
+    if fixes:
+      self.maps.extend(fixes)
 
   def __repr__(self):
     return f'<{self.src} to {self.dest}> {self.maps}'
@@ -100,8 +92,8 @@ def SplitRange(t_range, self_maps):
   maps = sorted(self_maps, key=lambda x: x.src_start)
   start_points = [i.src_start for i in maps]
   last_map = maps[-1]
-  # add one more point to mark the beginning of the unmapped section after
-  # the last range
+
+  # add one more point to mark the beginning of the last unmapped section
   start_points.append(last_map.src_start + last_map.range_len)
 
   start, length = t_range
@@ -173,47 +165,25 @@ def FullTransform(transformers, src_type, dest_type, num):
   return next_num
 
 
-def FullTransformRange(transformers, src_type, dest_type, t_ranges, print=Nop):
+def FullTransformRange(transformers, src_type, dest_type, t_ranges):
   """Given a dictionary of transformers, source & destination types, and a
      list of ranges, return what the number transforms into at the
      destination type."""
-  print(f'\nFull transform: {src_type} to {dest_type}')
   next_type = transformers[src_type].dest
-  print(f'\n{src_type} to {next_type}')
 
   # split each range if necessary
   self_map = transformers[src_type].maps
   split_ranges = []
   for t_range in t_ranges:
     split_subranges = SplitRange(t_range, self_map)
-    print(f't_range: {t_range} split to {split_subranges}')
     split_ranges.extend(split_subranges)
-  print(f'input ranges: {split_ranges}')
 
   # transform each range
   next_ranges = transformers[src_type].TransformRange(split_ranges)
-  print(f'next_ranges: {next_ranges}')
 
   if next_type == dest_type:
     return next_ranges
   return FullTransformRange(transformers, next_type, dest_type, next_ranges)
-
-
-def PrintTransformersDict(transformers):
-  """Given a dictionary of transformers, print them out nicely with the maps
-     in order by the source start."""
-  for t, v in transformers.items():
-    print(f'{t}: {v.src} to {v.dest}')
-    for m in sorted(v.maps, key=lambda x: x.src_start):
-      end = m.src_start + m.range_len
-      print(f'  {m} (next start: {end})')
-    fix_ranges = v.RangesNotContiguous()
-    if not fix_ranges:
-      print(f'  ranges contiguous')
-    else:
-      print(f'  ranges not contiguous; fix with:')
-      for f in fix_ranges:
-        print(f'  {f} (next start: {f.src_start + f.range_len})')
 
 
 def Part1(lines):
@@ -234,9 +204,7 @@ def Part2(lines):
 
   # fix all the transformer maps to eliminate gaps
   for t in transformers:
-    fixes = transformers[t].RangesNotContiguous()
-    if fixes:
-      transformers[t].maps.extend(fixes)
+    transformers[t].MakeRangesContiguous()
 
   output = FullTransformRange(transformers, 'seed', 'location', seed_ranges)
 
